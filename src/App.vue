150cf8f837feed1e299b143af311460f7f97d1c7
shijuhong <940104683@qq.com>
2022-05-16 20:27:16 +0800

    底部栏 docker 布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="docker">
+    <!-- class 写法：block__element--Modifier -->
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6b8;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe60a;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe78b;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
     }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &__title {
+    font-size: 0.2rem;
+    // 横向和纵向都缩放一半
+    transform: scale(0.5, 0.5);
+    // transform 中心点为：中心的顶部
+    transform-origin: center top;
   }
 }
 </style>
