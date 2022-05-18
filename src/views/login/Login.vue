440a0566ac3eea793fba682cfd7d85a66f2eb49c
shijuhong <940104683@qq.com>
2022-05-18 22:19:35 +0800

    登录页面布局开发

---
+++ src/views/login/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="wrapper">
+    <img
+      src="http://www.dell-lee.com/imgs/vue3/user.png"
+      alt=""
+      class="wrapper__img"
+    />
+    <div class="wrapper__input">
+      <input
+        type="text"
+        class="wrapper__input__content"
+        placeholder="请输入手机号"
+      />
+    </div>
+    <div class="wrapper__input">
+      <input
+        type="text"
+        class="wrapper__input__content"
+        placeholder="请输入密码"
+      />
+    </div>
+    <div class="wrapper__login-button">登录</div>
+    <div class="wrapper__login-link">立即注册</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Login",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/variable.scss";
+
+.wrapper {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  transform: translateY(-50%);
+  &__img {
+    display: block;
+    margin: 0 auto 0.4rem auto;
+    width: 0.66rem;
+    height: 0.66rem;
+  }
+  &__input {
+    box-sizing: border-box;
+    height: 0.48rem;
+    margin: 0 0.4rem 0.16rem 0.4rem;
+    padding: 0 0.16rem;
+    background: #f9f9f9;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 0.06rem;
+    &__content {
+      border: none;
+      outline: none;
+      width: 100%;
+      line-height: 0.48rem;
+      background: none;
+      color: $content-notice-fontcolor;
+      font-size: 0.16rem;
+      &::placeholder {
+        color: $content-notice-fontcolor;
+      }
+    }
+  }
+  &__login-button {
+    margin: 0.32rem 0.4rem 0.16rem 0.4rem;
+    line-height: 0.48rem;
+    font-size: 0.16rem;
+    background: #0091ff;
+    box-shadow: 0 0.04rem 0.08rem 0 rgba(0, 145, 255, 0.32);
+    border-radius: 0.04rem;
+    border-radius: 0.04rem;
+    color: #ffffff;
+    text-align: center;
+  }
+  &__login-link {
+    text-align: center;
+    font-size: 0.14rem;
+    color: $content-notice-fontcolor;
+  }
+}
+</style>
